77d64dcc1654978d77025d68a1dcce06a7639df5
zayitloud <williez@gmail.com>
2020-02-10 23:27:14 +0100

    beauty

--- service/calculator.js
+++ service/calculator.js
@@ -7,6 +7,6 @@ const getWord = (value) => {
     if (isNaN(value)) throw new Error(`Invalid number provided ${value}`)
     if (('' + value).includes('3')) return 'lucky'
     const word = multiples.map((mul, i) => isMultipleOf(value, mul) ? words[i] : NO_MATCH).filter(w => w != NO_MATCH)
-    return word ? (word.length == 0 ? value : word[0]) : value
+    return !word || word.length == 0 ? value : word[0]
 }
 module.exports = { getWord }
